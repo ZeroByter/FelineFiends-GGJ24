c0148e120aa858d0bed305a234c9358333122633
NeoThePotato <neorayb@gmail.com>
2024-01-25 21:16:28 +0200

    Made main menu functional.

---
+++ Assets/Scripts/Management/GameManager.cs
@@ -0,0 +1,36 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Management
+{
+	public class GameManager : MonoBehaviour
+	{
+		public static GameManager Instance { get; private set; }
+
+		private void Awake()
+		{
+			if (Instance == null)
+			{
+				Instance = this;
+				DontDestroyOnLoad(this);
+			}
+		}
+
+		public static void StartGame()
+		{
+			SceneManager.LoadSceneAsync("Level1", LoadSceneMode.Single);
+		}
+
+		public static void ExitToMainMenu()
+		{
+			DOTween.KillAll();
+			SceneManager.LoadSceneAsync("Main Menu", LoadSceneMode.Single);
+		}
+
+		public static void ExitGame()
+		{
+			Application.Quit();
+		}
+	}
+}
