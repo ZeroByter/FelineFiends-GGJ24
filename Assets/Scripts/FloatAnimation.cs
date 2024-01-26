77a58e475b3c1c81cd05a821ecf08433871a7d6a
NeoThePotato <neorayb@gmail.com>
2024-01-26 15:39:48 +0200

    Exported Paper Collectible animation to "FloatAnimation.cs".

---
+++ Assets/Scripts/FloatAnimation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+class FloatAnimation : MonoBehaviour
+{
+	[SerializeField] private float _movement = 0.5f;
+	[SerializeField] private float _duration = 1f;
+
+	private void OnEnable() => transform.DOLocalMoveY(_movement, _duration).SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo);
+
+	private void OnDisable() => DOTween.Kill(transform);
+}
