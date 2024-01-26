174b456c02b2218afd2ed68b43e3d65c4ee6c2da
NeoThePotato <neorayb@gmail.com>
2024-01-26 07:38:11 +0200

    Fixed PlayerJumpPredictor breaking when leaping off wall.

--- Assets/Scripts/InGame/Player/PlayerJumpPredictor.cs
+++ Assets/Scripts/InGame/Player/PlayerJumpPredictor.cs
@@ -9,6 +9,7 @@ namespace InGame.Player
 		[SerializeField] private LineRenderer lineRenderer;
 		[SerializeField] private int steps;
 		[SerializeField] private float distance;
+		[SerializeField] private float defaultGravityScale = 1;
 
 		private Vector2 JumpVelocity => jump.JumpVelocity;
 		private Transform JumpRoot => jump.transform;
@@ -16,6 +17,8 @@ namespace InGame.Player
 		private void OnValidate()
 		{
 			lineRenderer.positionCount = steps;
+			if (playerRb != null)
+				defaultGravityScale = playerRb.gravityScale;
 		}
 
 		private void Update()
@@ -27,7 +30,7 @@ namespace InGame.Player
 
 		private Vector2 GetPosition(float t)
 		{
-			return (Vector2)JumpRoot.position + (t * JumpVelocity) + 0.5f * t * t * Physics2D.gravity * playerRb.gravityScale;
+			return (Vector2)JumpRoot.position + (t * JumpVelocity) + 0.5f * t * t * Physics2D.gravity * defaultGravityScale;
 		}
 	}
 }
