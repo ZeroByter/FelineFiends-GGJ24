dc2e5087d08695d68a8492ed5d21a51ecc8318d7
NeoThePotato <neorayb@gmail.com>
2024-01-27 10:22:35 +0200

    Added paper collect VFX.

--- Assets/Scripts/InGame/Collectibles/PaperCollectible.cs
+++ Assets/Scripts/InGame/Collectibles/PaperCollectible.cs
@@ -1,14 +1,17 @@
 using InGame.UI;
+using UnityEngine;
 
 namespace InGame.Collectibles
 {
     public class PaperCollectible : BaseCollectible
     {
+        [SerializeField] private ParticleSystem vfx;
+
         override protected void OnCollected()
         {
             base.OnCollected();
-
             CollectedPapersCounter.AddToCounter();
+            vfx.Play();
         }
     }
 }
